1f556ebb6ae2cf6d29adfe97800676145767f47e
Amisha Gupta <amishagupta725@gmail.com>
2020-07-31 14:43:03 +0530

    Final Changes

--- app.js
+++ app.js
@@ -52,6 +52,42 @@ app.get("/services", function (req, res) {
   res.render("service");
 });
 
+app.get("/addons", function (req, res) {
+  res.render("addon");
+});
+
+app.get("/agreement", function (req, res) {
+  res.render("agreement");
+});
+
+app.get("/phone", function (req, res) {
+  res.render("phone");
+});
+
+app.get("/verification", function (req, res) {
+  res.render("verification");
+});
+
+app.get("/location", function (req, res) {
+  res.render("location");
+});
+
+app.get("/locationform", function (Req, res) {
+  res.render("locform");
+});
+
+app.get("/paydetails", function (req, res) {
+  res.render("paydetails");
+});
+
+app.get("/offers", function (req, res) {
+  res.render("offers");
+});
+
+app.get("/payment", function (req, res) {
+  res.render("payment");
+});
+
 app.listen(3000, function () {
   console.log("Serve on Door is running successfully...");
 });
